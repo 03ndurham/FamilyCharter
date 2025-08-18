a796f6a5dd89f98799b52b6e78e516048d86067a
Nathan J. Durham <03ndurham@gmail.com>
2025-08-18 06:29:49 -0400

    Updated link and filled in missing menu item

--- streamlit-app.py
+++ streamlit-app.py
@@ -2,5 +2,8 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-pg = st.navigation([st.Page("charter-text.py", title="Charter"), st.Page("ammendments.py", title="Proposed Ammendments")])
+pg = st.navigation([st.Page("charter.py", title="Charter"), 
+                    st.Page("changes.py", title="Proposed Changes"),
+                    st.Page("agenda.py", title="Agenda"),
+                    st.Page("minutes.py", title="Meeting Minutes")])
 pg.run()
