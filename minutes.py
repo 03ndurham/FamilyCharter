17310af4e2f3372184b4ba5980f6a00324093d29
Nathan J. Durham <03ndurham@gmail.com>
2025-08-19 12:36:18 -0400

    Starting filling out Meeting Minutes

--- minutes.py
+++ minutes.py
@@ -5,5 +5,35 @@ st.markdown("""
 # Meeting Minutes
 
 
+
+---
+
+### 09 August 2025
+
+> Lucy Livermore, Carole Preston, Harry Cruikshank, Sherry Findell, William Cruikshank, Nathan Durham
+
+- Met to follow up on changes.
+- Discussed potential financial institutions to begin saving for legal costs.
+- Examined Trustees and decided we may want to elect alternative trustee members in case of vacancy, rather than allow the trustees to vote to fill a vacancy.
+
+---
+
+### 02 August 2025
+
+> Lucy Livermore, Carole Preston, Harry Cruikshank, William Cruikshank, Brooke Cruikshank, Nathan Durham
+
+- Proposed the idea to Lucy Livermore, Harry Cruikshank, and William Cruikshank
+- Decided we would want to pusue this as a family.
+- Decided to add Widow / Widower clause in the trust.
+
+---
+
+### 26 July 2025
+
+> Carole Preston, Nathan Durham
+
+- Proposed the idea to Carole Preston.
+- Received feedback and edited draft charter
+
             
             """)
