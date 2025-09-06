b895aa761dc51afd21cd2d1dbadf997c920d55d8
Nathan J. Durham <03ndurham@gmail.com>
2025-09-06 19:33:16 -0400

    Updated meeting notes.

--- minutes.py
+++ minutes.py
@@ -8,6 +8,37 @@ st.markdown("""
 
 ---
 
+### 06 September 2025
+
+> Lucy Livermor (LL), Carole Preston (CP), Harry Cruikshank (HC), William Cruikshank (WC), Nathan Durham (ND)
+
+- Discussed the letter- Change due back date to October 15. Need to include a link to view the charter online.
+
+- Edited letter. 
+
+- **LL** is going to email family members.
+
+- After discussing the implecations of the proposed 'alternative trustee' system, we voted to not implement it with no dissenters.
+
+- **ND** Proposed New Sections 7.5, Definitions, 8.5, 8.7, 6.3, 6.4, 11.7. All approved with no dissenters.
+
+- **ND** Proposed Addition to Section 18.1. Approved with no dissenters.
+
+- **WC** Suggested editing time frame for proposed changes to be sent to the family for a vote to quarterly to annually for 'non-emergency' votes. This should be re-evaluated after two years of the trusts founding. Agreed upon with no dissenters.
+
+- **ND** Proposed New Section 3.5. Approved with no dissenters with the following change: Distributions are forfeit and reinvested as opposed to held in reserve.
+
+- **ND** Proposed New Section 8.6. Approved with no dissenters with the following change: Just publish in Financial Records available to all members. No requirement to send and notify voting-eligible members. This must be done by January 31st.
+
+- **ND** Proposed New Section 8.8. Approved with no dissenters with the following change: Alerts should only be triggered in the event of a Withdrawal or Transfer out of the account.
+
+- **CP** Proposed Changes to Section 3.2 and 3.1 to clarify that opting out does not entitle the individual to a refund of prior contributions. Once a contribution is made, it belongs to the trust. Approved with no dissenters.
+
+- **WC** proposed ammending Section 4.1 to specify that the Seeding Years should be 40 years or until the value of the trust reaches $500,000.00, whichever comes first. All in favor with no dissenters.
+
+
+---
+
 ### 09 August 2025
 
 > Lucy Livermore, Carole Preston, Harry Cruikshank, Sherry Findell, William Cruikshank, Nathan Durham
