34185373faadb18ac756b7407661d1be77711347
Nathan J. Durham <03ndurham@gmail.com>
2025-09-06 20:55:17 -0400

    Updated Agenda for next meeting

--- agenda.py
+++ agenda.py
@@ -5,13 +5,6 @@ st.markdown("""
 # Meeting Agenda
 
 ### Old Business
-1. Letter to Founding Members
-2. Trustee Governance Change - Alternatives 
-    - Do we change voting so that all trustee seats are rotated at the same time?
-    - Do we keep the current system where trustee seats are staggered, and allow trustees to vote to fill vacant seats?
-    - Do we do something else?
-    
+1. Receive photo for Trust Letterhead.
 
-### New Business
-1. Vote on Proposed Changes
             """)
