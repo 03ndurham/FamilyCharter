7fda6c57f61eea6334a5a4904a283947d1cbc05e
03ndurham <51920771+03ndurham@users.noreply.github.com>
2026-06-09 20:02:55 -0400

    Update charter.py
    Corrected type 'Amendemnts' to 'Amendments'

--- charter.py
+++ charter.py
@@ -815,7 +815,7 @@ Trustees and Stewards are empowered to adopt new tools, systems, or platforms fo
 
 ### Section 18.1 – General Amendment Procedure
 
-Any enrolled member may propose an amendment to this Charter. All proposed amendments must be submitted in writing and discussed in a public family forum (digital or in-person). Proposed Amendemnts must circulated **30 days** prior to the vote with the exact text and a summary of intent.
+Any enrolled member may propose an amendment to this Charter. All proposed amendments must be submitted in writing and discussed in a public family forum (digital or in-person). Proposed Amendments must circulated **30 days** prior to the vote with the exact text and a summary of intent.
 
 Amemdments shall be voted on no more than once per quarter.
  
